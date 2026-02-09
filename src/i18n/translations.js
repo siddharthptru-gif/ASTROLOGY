427d61b2aabef2e5a8c438f76dca285c77d5fec2
siddharthptru-gif <siddharthptru@gmail.com>
2026-02-09 21:14:03 +0530

    Create translations.js

---
+++ src/i18n/translations.js
@@ -0,0 +1,20 @@
+/**
+ * 🌍 TRANSLATIONS AGGREGATOR
+ * Combines all language files into a single resource object.
+ */
+
+import en from './en';
+import hi from './hi';
+import es from './es';
+import fr from './fr';
+import it from './it';
+import ko from './ko';
+
+export const translations = {
+  en, // English (Default)
+  hi, // Hindi
+  es, // Spanish
+  fr, // French
+  it, // Italian
+  ko, // Korean
+};
