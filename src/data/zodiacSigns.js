d02320e0fa46142b6f01e89884d02c3e80148ce8
siddharthptru-gif <siddharthptru@gmail.com>
2026-02-09 21:17:37 +0530

    Create zodiacSigns.js

---
+++ src/data/zodiacSigns.js
@@ -0,0 +1,115 @@
+/**
+ * ♈ ZODIAC SIGNS DATA
+ * Metadata for all 12 astrological signs.
+ */
+
+export const ZODIAC_SIGNS_DATA = [
+  {
+    id: 'aries',
+    name: 'Aries',
+    symbol: '♈',
+    element: 'Fire',
+    dateRange: 'Mar 21 - Apr 19',
+    planet: 'Mars',
+    traits: ['Courageous', 'Determined', 'Confident', 'Enthusiastic']
+  },
+  {
+    id: 'taurus',
+    name: 'Taurus',
+    symbol: '♉',
+    element: 'Earth',
+    dateRange: 'Apr 20 - May 20',
+    planet: 'Venus',
+    traits: ['Reliable', 'Patient', 'Practical', 'Devoted']
+  },
+  {
+    id: 'gemini',
+    name: 'Gemini',
+    symbol: '♊',
+    element: 'Air',
+    dateRange: 'May 21 - Jun 20',
+    planet: 'Mercury',
+    traits: ['Adaptable', 'Outgoing', 'Intelligent', 'Curious']
+  },
+  {
+    id: 'cancer',
+    name: 'Cancer',
+    symbol: '♋',
+    element: 'Water',
+    dateRange: 'Jun 21 - Jul 22',
+    planet: 'Moon',
+    traits: ['Tenacious', 'Imaginative', 'Loyal', 'Emotional']
+  },
+  {
+    id: 'leo',
+    name: 'Leo',
+    symbol: '♌',
+    element: 'Fire',
+    dateRange: 'Jul 23 - Aug 22',
+    planet: 'Sun',
+    traits: ['Creative', 'Passionate', 'Generous', 'Warm-hearted']
+  },
+  {
+    id: 'virgo',
+    name: 'Virgo',
+    symbol: '♍',
+    element: 'Earth',
+    dateRange: 'Aug 23 - Sep 22',
+    planet: 'Mercury',
+    traits: ['Loyal', 'Analytical', 'Kind', 'Hardworking']
+  },
+  {
+    id: 'libra',
+    name: 'Libra',
+    symbol: '♎',
+    element: 'Air',
+    dateRange: 'Sep 23 - Oct 22',
+    planet: 'Venus',
+    traits: ['Cooperative', 'Diplomatic', 'Gracious', 'Fair-minded']
+  },
+  {
+    id: 'scorpio',
+    name: 'Scorpio',
+    symbol: '♏',
+    element: 'Water',
+    dateRange: 'Oct 23 - Nov 21',
+    planet: 'Pluto',
+    traits: ['Resourceful', 'Brave', 'Passionate', 'Stubborn']
+  },
+  {
+    id: 'sagittarius',
+    name: 'Sagittarius',
+    symbol: '♐',
+    element: 'Fire',
+    dateRange: 'Nov 22 - Dec 21',
+    planet: 'Jupiter',
+    traits: ['Generous', 'Idealistic', 'Great sense of humor']
+  },
+  {
+    id: 'capricorn',
+    name: 'Capricorn',
+    symbol: '♑',
+    element: 'Earth',
+    dateRange: 'Dec 22 - Jan 19',
+    planet: 'Saturn',
+    traits: ['Responsible', 'Disciplined', 'Self-control', 'Good managers']
+  },
+  {
+    id: 'aquarius',
+    name: 'Aquarius',
+    symbol: '♒',
+    element: 'Air',
+    dateRange: 'Jan 20 - Feb 18',
+    planet: 'Uranus',
+    traits: ['Progressive', 'Original', 'Independent', 'Humanitarian']
+  },
+  {
+    id: 'pisces',
+    name: 'Pisces',
+    symbol: '♓',
+    element: 'Water',
+    dateRange: 'Feb 19 - Mar 20',
+    planet: 'Neptune',
+    traits: ['Compassionate', 'Artistic', 'Intuitive', 'Gentle']
+  }
+];
